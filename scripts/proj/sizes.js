74b55c25c3696740340d85b0a6987c26e4135e28
unknown <maximkulakivsky@gmail.com>
2022-04-19 03:48:27 +0300

    fix: main images, icons scripts

--- scripts/proj/sizes.js
+++ scripts/proj/sizes.js
@@ -7,13 +7,13 @@ function resizeTableIcons() {
     if (vwidth < 930) {
         let wrapWidth = document.querySelectorAll(".icons-table-wrap")[0].offsetWidth;
         let items = document.getElementsByClassName("icon-table-item");
+        const marginPercent = parseFloat(getComputedStyle(items.item(0)).margin.replace('%','')) / 100;
+        let itemsAmountInRow = Math.round(wrapWidth / 90)
+        let itemWidth = Math.floor( wrapWidth/itemsAmountInRow  - 2 * marginPercent * wrapWidth )
         for (let i = 0; i < items.length; i++) {
-            const marginPercent = items.item(i).style.margin;
-            let itemsAmountInRow = Math.round(wrapWidth / 90)
-            let itemWidth = wrapWidth/itemsAmountInRow  - 2 * marginPercent * wrapWidth
             items.item(i).style.width = itemWidth
-            iconTableSizeChanged = true
         }
+        iconTableSizeChanged = true
     } else if (iconTableSizeChanged) {
         let items = document.getElementsByClassName("icon-table-item");
         for (let i = 0; i < items.length; i++) {
